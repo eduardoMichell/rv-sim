cd25fe1a9a4cdf0d36df3a279d976495b1056555
eduardoMichell <eduardomichelgor@hotmail.com>
2025-04-03 21:19:30 -0300

    fix: fixing bollean write data in regFile

--- src/app/core/utils/riscv/regFile.ts
+++ src/app/core/utils/riscv/regFile.ts
@@ -1,33 +1,32 @@
 import { binaryToDecimal } from '../riscv-utils';
 
 export class RegFile {
-    registers: any;
-    constructor(regFile: any) {
-        this.registers = regFile;
+  registers: any;
+  constructor(regFile: any) {
+    this.registers = regFile;
+  }
+
+  writeRegister(index: string, value: string) {
+    this.registers[index] = value;
+  }
+
+  readRegister(index: string) {
+    return this.registers[index];
+  }
+
+  read(rd: string) {
+    return this.readRegister('x' + binaryToDecimal(rd));
+  }
+
+  write(regWrite: boolean, writeRg: string, data: string) {
+    if (typeof data === 'boolean') {
+      data = data ? '1' : '0';
     }
 
-    writeRegister(index: string, value: string) {
-        this.registers[index] = value;
+    if (regWrite && binaryToDecimal(writeRg) !== 0) {
+      this.writeRegister('x' + binaryToDecimal(writeRg), data);
+      return binaryToDecimal(writeRg);
     }
-
-    readRegister(index: string) {
-        return this.registers[index];
-    }
-
-    read(rd: string) {
-        return this.readRegister('x' + binaryToDecimal(rd));
-    }
-
-    write(regWrite: boolean, writeRg: string, data: string) {
-        if (regWrite && binaryToDecimal(writeRg) !== 0) {
-            this.writeRegister('x' + binaryToDecimal(writeRg), data);
-            return binaryToDecimal(writeRg);
-        }
-        return -1;
-    }
+    return -1;
+  }
 }
-
-
-
-
-
