06efcf562921886df3723c962f7a4d5a28e2d93b
eduardoMichell <eduardomichelgor@hotmail.com>
2025-04-15 21:34:29 -0300

    fix: fixing text editor canvas rendering error

--- src/app/components/runtime/runtime.component.ts
+++ src/app/components/runtime/runtime.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  HostListener,
+  OnDestroy,
+  OnInit,
+  ViewEncapsulation,
+} from '@angular/core';
 import { MatTabChangeEvent } from '@angular/material/tabs';
 import { Subscription, combineLatest } from 'rxjs';
 import { Asm, Code } from 'src/app/core/utils/types';
@@ -7,17 +13,22 @@ import { RuntimeService } from 'src/app/services/runtime-service/runtime.service
 import { convertWrittenCode } from 'src/app/core/utils/riscv-utils';
 import { ButtonsService } from 'src/app/services/buttons-service/buttons.service';
 import { UtilsService } from 'src/app/services/utils-service/utils.service';
+import * as monaco from 'monaco-editor';
 
 @Component({
   selector: 'app-runtime',
   templateUrl: './runtime.component.html',
   styleUrls: ['./runtime.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class RuntimeComponent implements OnInit, OnDestroy {
-
-  editorOptions = { theme: 'rv32i-theme', language: 'rv32i', minimap: { enabled: false }, };
+  editorOptions = {
+    theme: 'rv32i-theme',
+    language: 'rv32i',
+    minimap: { enabled: false },
+  };
   private codeSubscription: Subscription;
+  private editorInstance?: monaco.editor.IStandaloneCodeEditor;
 
   convertedCode: Asm;
   code: string = '.data\n\n.text\n';
@@ -33,23 +44,38 @@ export class RuntimeComponent implements OnInit, OnDestroy {
   ) {
     this.convertedCode = this.codeService.getConvertedCode();
     this.code = this.codeService.getCode();
-    this.codeSubscription = combineLatest([this.codeService.convertedCode$, this.codeService.code$]).subscribe(([convertedCode, code]) => {
+    this.codeSubscription = combineLatest([
+      this.codeService.convertedCode$,
+      this.codeService.code$,
+    ]).subscribe(([convertedCode, code]) => {
       this.convertedCode = convertedCode;
-      if (code != "") {
+      if (code != '') {
         this.code = code;
       }
-    })
+    });
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   ngOnDestroy() {
     this.codeSubscription.unsubscribe();
   }
 
+  onEditorInit(editor: monaco.editor.IStandaloneCodeEditor) {
+    this.editorInstance = editor;
+    setTimeout(() => this.editorInstance?.layout(), 50);
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize() {
+    this.editorInstance?.layout();
+  }
+
   onTabChange(event: MatTabChangeEvent) {
     this.runtimeService.setCurrentTabIndex(event.index);
+    if (event.index === 0) {
+      setTimeout(() => this.editorInstance?.layout(), 50);
+    }
   }
 
   getSelectedIndex(): number {
